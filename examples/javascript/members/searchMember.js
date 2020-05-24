37ebbbfb9a9d1def312135c4fa05836727415a4f
Al <rapidograph.il@gmail.com>
2020-05-24 23:34:41 +0300

    Minnor changes for local testing

--- examples/javascript/members/searchMember.js
+++ examples/javascript/members/searchMember.js
@@ -1,13 +1,21 @@
-var apiKey = process.env.TRELLO_API_KEY || 'YOUR_API_KEY';
-var oauthToken = process.env.TRELLO_OAUTH_TOKEN || 'OAUTH_TOKEN';
+require("dotenv").config();
+console.log(process.env);
 
-var Trello = require('../../../lib/trello-node-api')(apiKey, oauthToken);
+var apiKey = process.env.TRELLO_API_KEY;
+var oauthToken = process.env.TRELLO_OAUTH_TOKEN;
+
+var Trello = require("../../../lib/trello-node-api")(apiKey, oauthToken);
+
+console.log(apiKey);
 
 var memberRequest = function () {
-    Trello.member.search('MEMBER_ID').then(function (response) {
-        console.log('response ', response);
-    }).catch(function (error) {
-        console.log('error', error);
+  Trello.member
+    .search("5ec94046b8bb975a797a597e")
+    .then(function (response) {
+      console.log("response ", response);
+    })
+    .catch(function (error) {
+      console.log("error", error);
     });
 };
 
